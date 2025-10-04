6f61c2aa610eff828e8493505b3d21511fcd9126
Yusup <yusuprd@icloud.com>
2025-10-04 17:33:10 +0500

    Added post model

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    text = models.CharField(max_length=240)
+
+    def __str__(self):
+        return self.text
