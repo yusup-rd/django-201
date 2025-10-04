1ddb49e1b49b806c6475e832a933b148f83bc679
Yusup <yusuprd@icloud.com>
2025-10-04 18:36:22 +0500

    Added home page ListView

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+
+class HomePage(ListView):
+    http_method_names = ['get']
+    template_name = 'homepage.html'
+    model = Post
+    context_object_name = 'posts'
+    queryset = Post.objects.all().order_by('-id')[0:30]
